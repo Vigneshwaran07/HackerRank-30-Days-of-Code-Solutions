55d099bdb50c57a5e72944d060aa964c55bb4f00
Vigneshwaran V <43519088+Vigneshwaran07@users.noreply.github.com>
2018-09-24 00:31:04 +0530

    Create Day 14 : Scope.py

---
+++ Day 14 : Scope.py
@@ -0,0 +1,13 @@
+class Difference:
+    def __init__(self, a):
+        self.__elements = a
+    def computeDifference(self):
+        self.maximumDifference=max(a)-min(a)
+
+n = input()
+a = [int(e) for e in input().split(' ')]
+
+d = Difference(a)
+d.computeDifference()
+
+print(d.maximumDifference)        
