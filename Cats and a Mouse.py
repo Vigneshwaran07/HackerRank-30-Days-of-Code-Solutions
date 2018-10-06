9bb04b2a3bb6d17393ed617cbd2acab353b25048
Vigneshwaran V <43519088+Vigneshwaran07@users.noreply.github.com>
2018-10-06 10:28:04 +0530

    Delete Cats and a Mouse.py

--- Cats and a Mouse.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-for i in range(n):
-    a,b,c = (map(int, input().split()))
-    if(abs(c-a) > abs(c-b)):
-        print("Cat B")
-    if(abs(c-a) < abs(c-b)):
-        print("Cat A")
-    if(abs(c-a) == abs(c-b)):
-        print("Mouse C") 
